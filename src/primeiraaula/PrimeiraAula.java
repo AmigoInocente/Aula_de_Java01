a0eb92142f6a8995cf4329cd56ba1cd7cdb2548d
ALISSON GUILHERME FORMENTO <alisson_formento@estudante.sesisenai.org.br>
2023-07-11 21:16:44 -0300

    primeira aula

---
+++ src/primeiraaula/PrimeiraAula.java
@@ -0,0 +1,17 @@
+package primeiraaula;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alisson_formento
+ */
+public class PrimeiraAula {
+
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "HELLO WORLD");
+    }
+    
+    
+}
